c5cb869096605e9dde6068c7e049c1cff1201ae4
EHMD28 <ehmdiagne0530@gmail.com>
2025-12-24 20:43:56 -0500

    added material theme

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,28 @@
 <script lang="ts">
-	import favicon from '$lib/assets/favicon.svg';
+    import favicon from "$lib/assets/favicon.svg";
+    import "../app.css";
 
-	let { children } = $props();
+    let { children } = $props();
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
+    <link rel="icon" href={favicon} />
 </svelte:head>
 
-{@render children()}
+<header class="light">
+    <h2>Home</h2>
+    <h2>Listings</h2>
+    <h2>Custom</h2>
+    <h2>About</h2>
+</header>
+<main class="light">
+    {@render children()}
+</main>
+<footer class="light">
+    <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quos saepe
+        dolorem provident culpa distinctio ea quis voluptates molestias
+        possimus, perspiciatis nam fugit nulla soluta quia consequuntur deleniti
+        deserunt omnis? Eaque.
+    </p>
+</footer>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,48 @@
-<h1>Truly Forever Gems</h1>
-<p>Welcome</p>
+<h1 class="light">Truly Forever Gems</h1>
+<p class="light">
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt fugiat
+    est, mollitia eius officiis quo asperiores! Ab vitae, ipsum amet quae
+    eveniet ipsam repudiandae provident nisi, rem blanditiis, non officiis?
+    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae laudantium
+    repudiandae voluptatum reiciendis autem officiis recusandae, ipsam maxime
+    maiores eius odit. Adipisci porro commodi eius excepturi. Harum iste
+    quisquam officia! Lorem ipsum dolor sit amet consectetur adipisicing elit.
+    Quis voluptatum sapiente similique magni. Ratione enim dolorum nemo illum.
+    Odio, doloremque. At blanditiis atque cumque repellat dignissimos laudantium
+    laboriosam dolores voluptates.
+</p>
+<br />
+<p class="light">
+    Doloremque magni eveniet excepturi atque architecto natus ad, id ipsum
+    veritatis eligendi beatae quo ipsa harum, modi magnam dolore temporibus
+    dignissimos voluptates illo laudantium. Sint quos possimus ipsa delectus
+    consequuntur. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae
+    laudantium repudiandae voluptatum reiciendis autem officiis recusandae,
+    ipsam maxime maiores eius odit. Adipisci porro commodi eius excepturi. Harum
+    iste quisquam officia! Lorem ipsum dolor sit amet consectetur adipisicing
+    elit. Quis voluptatum sapiente similique magni. Ratione enim dolorum nemo
+    illum. Odio, doloremque. At blanditiis atque cumque repellat dignissimos
+    laudantium laboriosam dolores voluptates.
+</p>
+<br />
+<p class="light">
+    Quidem at libero deserunt itaque perferendis impedit. Numquam, vel obcaecati
+    aliquam nam necessitatibus veritatis fuga consectetur iusto ratione.
+    Delectus perspiciatis, illum eius eos dolore omnis rem minima eaque corporis
+    autem. Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati
+    alias quibusdam ratione quis, eligendi eos aut nostrum at, facilis, earum
+    quae totam pariatur. A non nobis fugiat iste placeat reprehenderit.
+</p>
+<br />
+<p class="light">
+    Ad, quos voluptates. Consequatur delectus tenetur dolores laboriosam
+    provident, modi quos esse doloribus totam facere reprehenderit
+    exercitationem saepe similique repellat! Aut pariatur sequi qui et accusamus
+    repellat quo adipisci omnis! Lorem, ipsum dolor sit amet consectetur
+    adipisicing elit. Quae laudantium repudiandae voluptatum reiciendis autem
+    officiis recusandae, ipsam maxime maiores eius odit. Adipisci porro commodi
+    eius excepturi. Harum iste quisquam officia! Lorem ipsum dolor sit amet
+    consectetur adipisicing elit. Quis voluptatum sapiente similique magni.
+    Ratione enim dolorum nemo illum. Odio, doloremque. At blanditiis atque
+    cumque repellat dignissimos laudantium laboriosam dolores voluptates.
+</p>
