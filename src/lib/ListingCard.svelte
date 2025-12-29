c7fdef2fe4428aeacea1c3e038054f802ced81ea
EHMD28 <ehmdiagne0530@gmail.com>
2025-12-29 14:35:10 -0500

    displaying listings

---
+++ src/lib/ListingCard.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+    import { type Listing, TestListings } from "$lib";
+
+    const { name, productId, imageUrl, price }: Listing = $props();
+</script>
+
+<div class="listing-card dark">
+    <p>{name}</p>
+    <div class="image-container">
+        <img src={imageUrl} alt="image for {name}" />
+    </div>
+    <p>{price}</p>
+</div>
+
+<style>
+    p {
+        margin: 0;
+        padding: 0;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        font-size: 1.5rem;
+    }
+
+    .listing-card {
+        background-color: var(--md-sys-color-secondary-container);
+        overflow: hidden;
+    }
+
+    .image-container {
+        width: 100%;
+        aspect-ratio: 1 / 1;
+        overflow: hidden;
+    }
+
+    .image-container > img {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+        object-position: center;
+    }
+</style>
--- src/routes/listings/+page.svelte
+++ src/routes/listings/+page.svelte
@@ -1,3 +1,30 @@
+<script>
+    import { TestListings } from "$lib";
+    import ListingCard from "$lib/ListingCard.svelte";
+</script>
+
 <svelte:head>
     <title>Listings | Truly Forever Gems</title>
 </svelte:head>
+
+<div class="cards-container">
+    {#each TestListings as { name, productId, imageUrl, price }}
+        <ListingCard {name} {productId} {imageUrl} {price} />
+    {/each}
+</div>
+
+<style>
+    .cards-container {
+        margin: 0 5vw;
+
+        display: grid;
+        max-width: 1200px;
+        grid-template-columns: repeat(
+            auto-fit,
+            minmax(clamp(160px, 25vw, 240px), 1fr)
+        );
+        justify-items: stretch;
+        column-gap: 2vw;
+        row-gap: 2vh;
+    }
+</style>
