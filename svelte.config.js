4166a2f0be91943f49071c312f17c6a2f6725480
EHMD28 <ehmdiagne0530@gmail.com>
2026-01-01 18:39:26 -0500

    added vercel adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,6 +8,7 @@ const config = {
     preprocess: vitePreprocess(),
 
     kit: {
+        /* TODO: Configure image optimization (https://vercel.com/docs/build-output-api/configuration#images) */
         adapter: adapter(),
         paths: {
             base: '',
